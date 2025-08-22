b0b82b6e7512bf092bb177cd6796fdd76aa87ddd
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-08-22 13:30:10 -0300

    Add roteamento dinâmico para  card  components
    Os componentes BigCard e SmallCard foram atualizados para usar [routerLink] para navegação com base no ID do card. O ID foi passado como @Input do componente inicial e o parâmetro de rota foi tratado no ContentComponent para exibição dinâmica de conteúdo.

--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -10,9 +11,14 @@ export class ContentComponent implements OnInit {
   contentTitle:string = 'EXEMPLO BEM LEGAL';
   contentDescription:string = 'akshfjkagsfkjagfjksaf';
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value => {
+      console.log(value.get('id'))
+    })
   }
 
 }
